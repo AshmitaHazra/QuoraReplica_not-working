e22c42125beeda8309ac130c5aeba16e805800ca
AshmitaHazra <Aashi.hazra22@gmail.com>
2020-08-22 01:21:19 +0530

    Latest

---
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
@@ -0,0 +1,24 @@
+package com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.QuestionEntity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import java.util.List;
+
+public class QuestionDao {
+
+    private EntityManager entityManager;
+
+    public QuestionEntity createQuestion(QuestionEntity questionEntity){
+        entityManager.persist(questionEntity);
+        return questionEntity;
+    }
+    public List<QuestionEntity> getAllQuestion() {
+        try {
+            return entityManager.createNamedQuery("allQuestions", QuestionEntity.class).getResultList();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+}
